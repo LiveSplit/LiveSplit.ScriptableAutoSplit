93a100b1d645a55e45a54fd806752013e6591d1c
Fatalis <fatalis.twitch@gmail.com>
2015-08-20 22:30:17 -0500

    Catch script parse exception

--- Component.cs
+++ Component.cs
@@ -80,12 +80,19 @@ namespace LiveSplit.UI.Components
             // this is ugly, fix eventually!
             if (Settings.ScriptPath != OldScriptPath && !string.IsNullOrEmpty(Settings.ScriptPath) || DoReload)
             {
-                Script = ASLParser.Parse(File.ReadAllText(Settings.ScriptPath));
-                OldScriptPath = Settings.ScriptPath;
-                FSWatcher.Path = Path.GetDirectoryName(Settings.ScriptPath);
-                FSWatcher.Filter = Path.GetFileName(Settings.ScriptPath);
-                FSWatcher.EnableRaisingEvents = true;
-                DoReload = false;
+                try
+                {
+                    DoReload = false;
+                    Script = ASLParser.Parse(File.ReadAllText(Settings.ScriptPath));
+                    OldScriptPath = Settings.ScriptPath;
+                    FSWatcher.Path = Path.GetDirectoryName(Settings.ScriptPath);
+                    FSWatcher.Filter = Path.GetFileName(Settings.ScriptPath);
+                    FSWatcher.EnableRaisingEvents = true;
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(ex);
+                }
             }
 
             if (Script != null)
