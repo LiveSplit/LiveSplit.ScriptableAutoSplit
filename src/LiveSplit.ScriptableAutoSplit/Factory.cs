eb1fb42c1091dab74cbd48d361c1f14c89eb5715
Ero <imjustero@gmail.com>
2024-09-02 10:07:09 +0200

    Use file-scoped namespaces, move `using` directives, fix whitespace
    * `IDE0005`, `IDE0065`, `IDE0161`

--- src/LiveSplit.ScriptableAutoSplit/Factory.cs
+++ src/LiveSplit.ScriptableAutoSplit/Factory.cs
@@ -1,23 +1,23 @@
 ﻿using System;
+
 using LiveSplit.Model;
 using LiveSplit.UI.Components;
 
 [assembly: ComponentFactory(typeof(Factory))]
 
-namespace LiveSplit.UI.Components
+namespace LiveSplit.UI.Components;
+
+public class Factory : IComponentFactory
 {
-    public class Factory : IComponentFactory
-    {
-        public string ComponentName => "Scriptable Auto Splitter";
-        public string Description => "Allows scripts written in the ASL language to define the splitting behaviour.";
-        public ComponentCategory Category => ComponentCategory.Control;
-        public Version Version => Version.Parse("1.8.29");
+    public string ComponentName => "Scriptable Auto Splitter";
+    public string Description => "Allows scripts written in the ASL language to define the splitting behaviour.";
+    public ComponentCategory Category => ComponentCategory.Control;
+    public Version Version => Version.Parse("1.8.29");
 
-        public string UpdateName => ComponentName;
-        public string UpdateURL => "http://livesplit.org/update/";
-        public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.ScriptableAutoSplit.xml";
+    public string UpdateName => ComponentName;
+    public string UpdateURL => "http://livesplit.org/update/";
+    public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.ScriptableAutoSplit.xml";
 
-        public IComponent Create(LiveSplitState state) => new ASLComponent(state);
-        public IComponent Create(LiveSplitState state, string script) => new ASLComponent(state, script);
-    }
+    public IComponent Create(LiveSplitState state) => new ASLComponent(state);
+    public IComponent Create(LiveSplitState state, string script) => new ASLComponent(state, script);
 }
